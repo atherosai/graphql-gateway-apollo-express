c124a79f3078fab71b5f4c8207963fcbec1b91bb
a7v8x <david.mraz@gmail.com>
2017-11-14 02:24:59 +0100

    fix($DateTime)

--- server/graphql/scalars/dateTimeScalar.js
+++ server/graphql/scalars/dateTimeScalar.js
@@ -6,21 +6,21 @@ const parseISO8601 = (value) => {
   if (isISO8601(value)) {
     return value;
   }
-  throw new Error('DateTime cannot represent an invalid ISO-8601 Date instance');
+  throw new Error('DateTime cannot represent an invalid ISO-8601 Date string');
 };
 
 const serializeISO8601 = (value) => {
   if (isISO8601(value)) {
     return value;
   }
-  throw new Error('DateTime cannot represent an invalid ISO-8601 Date instance');
+  throw new Error('DateTime cannot represent an invalid ISO-8601 Date string');
 };
 
 const parseLiteralISO8601 = (ast) => {
   if (isISO8601(ast.value)) {
     return ast.value;
   }
-  throw new Error('DateTime cannot represent an invalid ISO-8601 Date instance');
+  throw new Error('DateTime cannot represent an invalid ISO-8601 Date string');
 };
 
 const DateTime = new GraphQLScalarType({
